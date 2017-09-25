fb7f5c5681534b61c75d76ebf4c8bb29d7cc6b23
bkneis <b.kneis2011@gmail.com>
2017-09-25 07:50:59 +0100

    Create helper module to reduce boilerplate graph code

---
+++ plotHelper.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+def plotPie(totals):
+    labels = totals.keys()
+    fracs = totals.values()
+    # Make square figures and axes
+    the_grid = GridSpec(1, 1)
+    plt.subplot(the_grid[0, 0], aspect=1)
+    plt.pie(fracs, labels=labels, autopct='%1.1f%%', shadow=True)
+    plt.show()
