a940d89641b191ec23de5ba36b6127c8fbc013e0
bkneis <b.kneis2011@gmail.com>
2017-09-26 14:12:08 +0100

    Add ability to resize the images based on the open cv examples and export the csv to a file instead of std out

--- create_csv.py
+++ create_csv.py
@@ -1,20 +1,26 @@
-#!/usr/bin/env python
-
 import sys
 import os.path
+from PIL import Image
 
-if __name__ == "__main__":
+SIZE = (1280, 720)
+IMAGE_EXT = '.png'
+SEPARATOR = ";"
 
-    if len(sys.argv) != 2:
-        print "usage: create_csv <base_path>"
-        sys.exit(1)
 
-    BASE_PATH=sys.argv[1]
-    SEPARATOR=";"
-    file = ""
+def resize_image(filepath):
+    image = Image.open(filepath)
+    new_image = image.resize(SIZE)
+    dir_path = os.path.dirname(os.path.abspath(filepath))
+    file_name = os.path.splitext(os.path.basename(filepath))[0]
+    new_filepath = '%s/%s%s' % (dir_path, file_name, IMAGE_EXT)
+    new_image.save(new_filepath)
+    return new_filepath
 
+
+def create_csv(base_path):
     label = 0
-    for dirname, dirnames, filenames in os.walk(BASE_PATH):
+    images = []
+    for dirname, dirnames, filenames in os.walk(base_path):
         for subdirname in dirnames:
             subject_path = os.path.join(dirname, subdirname)
             for dirname2, sessionDirs, subjectFiles in os.walk(subject_path):
@@ -22,6 +28,23 @@ if __name__ == "__main__":
                     session_path = os.path.join(subject_path, sessionDir)
                     for filename in os.listdir(session_path):
                         abs_path = "%s/%s" % (session_path, filename)
-                        print "%s%s%d" % (abs_path, SEPARATOR, label)
+                        filepath, file_extension = os.path.splitext(abs_path)
+                        if file_extension == '.bmp':
+                            abs_path = resize_image(abs_path)
+                            images.append("%s%s%d" % (abs_path, SEPARATOR, label))
+                            print("%s%s%d" % (abs_path, SEPARATOR, label))
             label += 1
+
+    outfile = open("./faces.csv", "w")
+    outfile.write("\n".join(images))
+    outfile.close()
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 2:
+        print("usage: create_csv <base_path>")
+        sys.exit(1)
+
+    create_csv(sys.argv[1])
 
