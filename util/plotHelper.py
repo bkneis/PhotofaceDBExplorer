493cfacf7f285b565a3dcec82556eda86805c156
bkneis <b.kneis2011@gmail.com>
2017-09-27 10:56:57 +0100

    Remove debug logs

--- util/plotHelper.py
+++ util/plotHelper.py
@@ -5,8 +5,6 @@ from matplotlib.gridspec import GridSpec
 def plotPie(totals):
     labels = totals.keys()
     fracs = totals.values()
-    print(labels)
-    print(fracs)
     # Make square figures and axes
     the_grid = GridSpec(1, 1)
     plt.subplot(the_grid[0, 0], aspect=1)
