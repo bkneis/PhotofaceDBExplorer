e5f8971b1b8e48d4ff42664423d2cb657d69f5c4
bkneis <b.kneis2011@gmail.com>
2017-09-27 10:29:45 +0100

    Move notebooks and util scripts to own folder

---
+++ util/plotHelper.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+
+def plotPie(totals):
+    labels = totals.keys()
+    fracs = totals.values()
+    print(labels)
+    print(fracs)
+    # Make square figures and axes
+    the_grid = GridSpec(1, 1)
+    plt.subplot(the_grid[0, 0], aspect=1)
+    plt.pie([float(v) for v in fracs], labels=[v for v in labels], autopct='%1.1f%%', shadow=True)
+    plt.show()
+
